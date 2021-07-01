e724c0f7c95332d9c12170cb27495f4665bf2a38
Gianna Giavarini <giannagiavarini@outlook.com>
2021-07-01 20:51:22 -0200

    ejercicio 2 JS

---
+++ index.js
@@ -0,0 +1,7 @@
+let sabor1 = prompt ("Ingrese gusto de helado")
+let sabor2 = prompt ("Ingrese gusto de helado")
+let sabor3 = prompt ("Ingrese gusto de helado")
+
+let saboresElegidos = (sabor1 + " " + sabor2 + " " + "y" + " " + sabor3)
+
+alert ("Aquí tiene su helado de " + saboresElegidos)
